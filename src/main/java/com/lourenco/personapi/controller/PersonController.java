dcaa6b93001d0e93d0b337650ede253d6211ef4c
ConceicaoLourenco <conceicaoLourenzo@gmail.com>
2021-03-07 23:25:31 -0300

    fix getMapping

--- src/main/java/com/lourenco/personapi/controller/PersonController.java
+++ src/main/java/com/lourenco/personapi/controller/PersonController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/person")
 public class PersonController {
 
-    GetMapping
+    @GetMapping
     public String getPerson() {
         return "person";
     }
